5a757d71a79414ec440470cf9273ef9ea2576ca1
Rocangus <hunter_sniper112@hotmail.com>
2019-11-11 15:59:50 +0100

    Added Bus

---
+++ GarageManagementSoftware/Vehicles/Bus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarageManagementSoftware.Vehicles
+{
+    public class Bus : Vehicle
+    {
+        public int PassengerCapacity { get; private set; }
+        public Bus(string registrationNumber, string color, int numberOfWheels, int emptyMass, int passengerCapacity) : base(registrationNumber, color, numberOfWheels, emptyMass)
+        {
+            PassengerCapacity = passengerCapacity;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + ", a bus";
+        }
+    }
+}
