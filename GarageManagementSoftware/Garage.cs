d53dcb055865169fd4cce86ab9a7d6647c4af733
Rocangus <hunter_sniper112@hotmail.com>
2019-11-11 09:48:43 +0100

    Sketched out Garage and abstract class Vehicle

---
+++ GarageManagementSoftware/Garage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using GarageManagementSoftware.Vehicles;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarageManagementSoftware
+{
+    public class Garage<T> where T : Vehicle
+    {
+        private Vehicle[] vehicles;
+        public int Capacity { get; private set; }
+        public int Count { get; private set; }
+        public Garage(int capacity) 
+        {
+            vehicles = new Vehicle[capacity];
+            Count = 0;
+        }
+
+    }
+}
---
+++ GarageManagementSoftware/Vehicles/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarageManagementSoftware.Vehicles
+{
+    public abstract class Vehicle
+    {
+        public string RegistrationNumber { get; protected set; }
+        public string Color { get; protected set; }
+        public int NumberOfWheels { get; protected set; }
+
+        public Vehicle(string regnumber, string color, int numberofwheels)
+        {
+            RegistrationNumber = regnumber;
+            Color = color;
+            NumberOfWheels = numberofwheels;
+        } 
+    }
+}
